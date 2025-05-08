e734f14bc22d1b7dda87b691c3d9d61d6d8809c9
ChengChangming <excelccm@gmail.com>
2025-05-08 19:17:46 +0800

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
 const Home = () => {
   return (
+    // <div
+    //   className="p-6 bg-gray-100 border b border-gray-300 rounded-lg shadow-lg
+    //  text-gray-700 text-lg font-semibold leading-relaxed tracking-wide text-center
+    //  max-w-sm mx-auto mt-10"
+    // >
+    //   Tailwind css is FUN!!
+    // </div>
+    // <h1 className="text-primary-500">Tailwind css is FUN!!</h1>
     <div>
-      <h1 className="text-3xl text-violet-700 font-black">
-        Welcome to Next.js 🖖
+      <h1 className="h1-bold">Tailwind css is FUN!!</h1>
+      <h1 className="h1-bold font-inter">Tailwind css is FUN!!(Inter)</h1>
+      <h1 className="h1-bold font-space-grotesk">
+        Tailwind css is FUN!!(Inter)
       </h1>
     </div>
   );
